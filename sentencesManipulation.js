7eb7c5e8ba329730f267b095d6a0c8268e0f824f
Nuralamsah Zulkarnaim <30795934+alamsahzul@users.noreply.github.com>
2017-09-27 22:24:05 +0700

    Update sentencesManipulation.js

--- sentencesManipulation.js
+++ sentencesManipulation.js
@@ -1,21 +1,21 @@
-function sentencesManipulation(kalimat)
+function sentencesManipulation(sentence)
 {
-  var tampung = kalimat.split(' ');
-  var hasil   = [];
-  for (i=0; i<tampung.length; i++) {
-    if (tampung[i].charAt(0)=='a' || tampung[i].charAt(0)=='i' || tampung[i].charAt(0)=='u' || tampung[i].charAt(0)=='e' || tampung[i].charAt(0)=='o' )
+  var container = sentence.split(' ');
+  var result   = [];
+  for (i=0; i<container.length; i++) {
+    if (container[i].charAt(0)=='a' || container[i].charAt(0)=='i' || container[i].charAt(0)=='u' || container[i].charAt(0)=='e' || container[i].charAt(0)=='o' )
     {
-      hasil.push(tampung[i]);
+      result.push(container[i]);
     }
     else
     {
-      var awal = tampung[i].charAt(0);
+      var awal = container[i].charAt(0);
       var tambah = 'nyo';
-      var hapus = tampung[i].substr(1);
-      hasil.push(hapus.concat(awal,tambah));
+      var hapus = container[i].substr(1);
+      result.push(hapus.concat(awal,tambah));
     }
     }
-      console.log(hasil.join(" "));
+      console.log(result.join(" "));
 }
 
-sentencesManipulation('ibu pergi ke pasar bersama aku');
+sentencesManipulation('my mother go to the market to buy some vegetables');
